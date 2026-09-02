ff2518b3be4643cd40be6749a183ce19b1a11aad
Ian Lu <iandragonlu99@hotmail.com>
2026-09-03 00:18:41 +0930

    Added group info to readme + started on cipher.py

---
+++ cipher.py
@@ -0,0 +1,6 @@
+# Question 1
+
+shift1 = int(input("Enter shift1 (must be a non negative-integer): "))
+shift2 = int(input("Enter shift2 (must be a non negative-integer): "))
+
+print(shift1, shift2)
