3cd7ffcd1065f4d51dfd43f3a0fc789298698040
Ian Lu <iandragonlu99@hotmail.com>
2026-09-09 23:28:19 +0930

    Added shift1 & shift2

--- cipher.py
+++ cipher.py
@@ -1,6 +1,21 @@
 # Question 1
 
-shift1 = int(input("Enter shift1 (must be a non negative-integer): "))
-shift2 = int(input("Enter shift2 (must be a non negative-integer): "))
+shift = [0,0]
+i = 0
+
+while i < 2:
+    user_input = input(f"Enter shift{i+1} (must be a non-negative integer): ")
+    try:
+        number = int(user_input)
+        if number >= 0:
+            shift[i] = number
+            i += 1
+        else:
+            print("Negative integer entered")
+    except ValueError:
+        print("ERROR (non-integer entered)")
+
+shift1 = shift[0]
+shift2 = shift[1]
 
 print(shift1, shift2)
